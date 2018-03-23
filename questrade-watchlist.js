8a3936de38681c49fb432be05518e42f6b15a1d1
Clinton Bale <clinton.bale@gmail.com>
2018-03-22 20:03:44 -0400

    First version
    Added version 1.0 code, updates watchlists every 5 seconds.

--- questrade-watchlist.js
+++ questrade-watchlist.js
@@ -1,1 +1,25 @@
+// ==UserScript==
+// @name         Questrade Watchlist Updater
+// @namespace    https://github.com/clintonbale/userscripts
+// @version      1.0
+// @description  Updates all Questrade watchlists periodically
+// @author       clintonbale
+// @match        https://my.questrade.com/trading/*
+// @grant        none
+// ==/UserScript==
 
+// refresh watchlists every 5 seconds
+var updateInterval = 5000;
+
+(function($) {
+    'use strict';
+    setInterval(function () {
+        $(".watch-list-view").each(function (i, o) {
+            var scope = angular.element(o).scope();
+            var snapper = scope.vmWatchListGadget || scope.vmWatchlistView;
+            if (snapper && typeof snapper.snapData === "function") {
+                snapper.snapData();
+            }
+        });
+    }, updateInterval);
+})(jQuery);
